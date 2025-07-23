692e13f6802b6291440c53381a10a8ac863102e2
MOHD MUQEEM NASIR <80195720+MuqeemNasir@users.noreply.github.com>
2025-07-23 13:29:32 +0000

    Changes to be committed: 	modified:   src/components/Header.jsx    Increases the Margin-Bottom in Website Heading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Header = () => (
   <header className="bg-dark text-light">
     <div className="container py-3">
-      <h1 className="display-3 fw-medium mb-1">My Food Delivery Website</h1>
+      <h1 className="display-3 fw-medium mb-3">My Food Delivery Website</h1>
       <nav>
         <ul className="nav">
           <li className="nav-item">
